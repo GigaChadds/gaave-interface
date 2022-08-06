46260ebceb77c3c81a76abfde583c828f15ffff6
Eric Mong <m.changhsi@gmail.com>
2022-08-07 04:25:47 +0900

    Update next.config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/mint',
-  //       destination: '/mint',
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: '/:path*',
-  //       destination: '/',
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
+  // Looks like backward compatibility approach.
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
+
+    return config;
+  },
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
+
+module.exports = nextConfig;
